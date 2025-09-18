84a7b26f438183a096eafb2c45484f66495128f7
Ayub40 <ayubk4028@gmail.com>
2025-09-18 18:33:57 +0600

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1 className='text-4xl'>About Page</h1>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/blog/[blogId]/page.tsx
@@ -0,0 +1,15 @@
+
+
+const DynamicBlogPage = async ({ params }: { params: Promise<{ blogId: string }> }) => {
+    // console.log(await params);
+    const { blogId } = await params;
+
+    return (
+        <div>
+            <h1 className='text-4xl'>Dynamic Blog Page BlogId: {blogId}</h1>
+        </div>
+    );
+};
+
+export default DynamicBlogPage;
+
