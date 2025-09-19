27fde47ad918c7829d05463d9aa56156adf8b4fc
Ayub40 <ayubk4028@gmail.com>
2025-09-20 02:23:32 +0600

    52-1 Optimize image using Image Component

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // images: {
+  //   remotePatterns: [new URL('https://innovar-orpin.vercel.app/assets/images/inner-pages/image-07.jpg')],
+  // },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+
 };
 
 export default nextConfig;
---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import nextImg from "../../assets/Rahe Jannat.jpg"
+
+const GalleryPage = () => {
+    return (
+        <div className="text-center">
+            <h1>Regular Image Tag</h1>
+            {/* <img src="https://www.zhuravkovigor.com/assests/next-js-optimizations.png" */}
+            <img src="https://innovar-orpin.vercel.app/assets/images/inner-pages/inner-img-02.jpg"
+                alt=""
+                width={500}
+                height={500}
+                className="mx-auto" />
+
+            {/* Next Js e Image set korar rules */}
+            <h1>NextJs Image Component</h1>
+            <Image src="https://innovar-orpin.vercel.app/assets/images/inner-pages/image-07.jpg"
+                alt="Next Image"
+                width={500}
+                height={500}
+                className="mx-auto" />
+
+            <h1>Local assests NextJs Image Component</h1>
+            <Image src={nextImg}
+                alt="Next Image"
+                width={500}
+                height={500}
+                className="mx-auto" />
+
+            <h1>Public NextJs Image Component</h1>
+            <Image src={nextImg}
+                // alt="/public/Rahe Jannat.jpg"
+                alt="/Rahe Jannat.jpg"
+                width={500}
+                height={500}
+                className="mx-auto" />
+        </div>
+    );
+};
+
+export default GalleryPage;
