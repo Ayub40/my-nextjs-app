456e64c5c3c689b077a12a6737cd0f74acf81d7d
Ayub40 <ayubk4028@gmail.com>
2025-09-22 02:22:35 +0600

    52-10 Module Summary and practice task

---
+++ src/app/posts/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostsLoadingPage = () => {
+    return (
+        <div className="min-h-dvh flex justify-center items-center">
+            <h1 className="text-5xl">Loading....</h1>
+        </div>
+    );
+};
+
+export default PostsLoadingPage;
